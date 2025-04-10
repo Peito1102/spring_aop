3ebd907bd37d995987a5973c940119a266155067
Peito1102 <renzitiwin@gmail.com>
2025-04-10 17:02:12 -0500

    parte 1,2 y 3 (aop 08)

---
+++ src/main/java/com/vasquez/springboot/aop/springboot_aop/controllers/GrettingController.java
@@ -0,0 +1,25 @@
+package com.vasquez.springboot.aop.springboot_aop.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import com.vasquez.springboot.aop.springboot_aop.services.GreetingService;
+
+import java.util.Collections;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@RestController
+public class GrettingController {
+
+    @Autowired
+    private GreetingService greeting;
+
+    @GetMapping("/greeting")
+    public ResponseEntity<?> greeting() {
+        return ResponseEntity.ok(Collections.singletonMap("greeting",greeting.sayHello("Pepito", "gou p mrd!")));
+    }
+    
+}
---
+++ src/main/java/com/vasquez/springboot/aop/springboot_aop/services/GreetingService.java
@@ -0,0 +1,5 @@
+package com.vasquez.springboot.aop.springboot_aop.services;
+
+public interface GreetingService {
+    String sayHello(String person, String phrase);
+}
